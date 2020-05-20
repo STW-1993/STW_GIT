c696e042e1a031a0c2af26f8b273ca2e1f54ba77
STW-1993 <hzwmail1993@163.com>
2020-05-20 11:54:44 +0800

    相关文件上传

--- 007_testmain.c
+++ 007_testmain.c
@@ -1,7 +1,6 @@
 /*
  * 用于测试RCTrace打印函数
  */
-#include <stdio.h>
 #include "007_level_printf.h"
 
 int main(int argc,char **argv)
@@ -9,9 +8,10 @@ int main(int argc,char **argv)
 	SetDebugLevel(atoi(argv[1]));  //字符转换为数字
 
 	char *p = "abcdefghijklmnopqrstuvwxyz";
+	char str[] = "hello world!";
 	int num = 888;
 	
-	RCTrace(5,"hello world! %d %s\n\r",num,p);
+	RCTrace(5,"%s %d %s\n\r",str,num,p);
 	
 	return 0;
 }
