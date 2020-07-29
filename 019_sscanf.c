aefcda979bb234ff65532884650ac15bb8d2bfd1
STW-1993 <hzwmail1993@163.com>
2020-07-29 10:03:31 +0800

    训练代码上传

--- 019_sscanf.c
+++ 019_sscanf.c
@@ -10,12 +10,18 @@ int main()
 	char str[] = "ABCDEFG/HI123456\rJK@LMN";
 	char tmp[50];
 	char tmp2[50];
+	char str2[] = "5~abcd";
+	int	num1, num2;
+	char *arr;
 
 	sscanf(str,"%*[^/]/%[^@]",tmp);
 	printf("%s \r\n",tmp);
 
 	sscanf(str,"%[^\r]",tmp2);
 	printf("%s \r\n",tmp2);
+
+	sscanf(str2,"%d~%s", &num1, arr);
+	printf("%d  %s \n\r", num1, arr);
 	
     return 0;
 
