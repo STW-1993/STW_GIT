4002b4302da36501aff1ce2361c58a3b26a14284
STW-1993 <734648964@qq.com>
2020-12-25 09:28:31 +0800

    优化选择输出二维数组第几大的数的下标的代码

--- 026_Output_array_index.c
+++ 026_Output_array_index.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	int i,j,k;
+	int i,j;
 	int tmp = 0;
 	int a;
 	
@@ -20,12 +20,14 @@ int main(void)
 	int arr_tmp[4][5] = {0};
 
 	memcpy(arr_tmp, arr, sizeof(arr));
-	
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 5; j++) {
-			printf("%d  ", arr[i][j]);
+
+	printf("原来的二维数组：\n\r");
+	for (i = 0; i < sizeof(arr)/sizeof(arr[0][0]); i++) {
+		printf("%d	", arr[i/5][i%5]);
+		
+		if (4 == i%5) {
+			printf("\n\r");
 		}
-		printf("\n\r");
 	}
 
 	printf("请输入要显示的以上4x5矩阵的第几大的数：");
@@ -41,19 +43,19 @@ int main(void)
 		}
 	}
 
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 5; j++) {
-			printf("%d	", arr[i][j]);
+	printf("原来的二维数组从高到低排列后为：\n\r");
+	for (i = 0; i < sizeof(arr)/sizeof(arr[0][0]); i++) {
+		printf("%d	", arr[i/5][i%5]);
+		
+		if (4 == i%5) {
+			printf("\n\r");
 		}
-		printf("\n\r");
 	}
 	printf("第%d大的数为：%d \n\r", a, arr[(a-1)/5][(a-1)%5]);
 
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 5; j++) {
-			if (arr_tmp[i][j] == arr[(a-1)/5][(a-1)%5]) {
-				printf("第%d大的数的下标为：arr[%d][%d]\n\r", a, i, j);
-			}
+	for (i = 0; i < sizeof(arr)/sizeof(arr[0][0]); i++) {
+		if (arr_tmp[i/5][i%5] == arr[(a-1)/5][(a-1)%5]) {
+			printf("第%d大的数在原来的二维数组下的坐标为：arr[%d][%d]\n\r", a, i/5, i%5);
 		}
 	}
 	return 0;
