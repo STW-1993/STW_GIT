77b45ea6bc25d85736760ee3be65c0f2b8f95825
STW-1993 <734648964@qq.com>
2021-04-22 14:24:00 +0800

    更新等级打印源码

--- 004_RCTrace/RCTrace.c
+++ 004_RCTrace/RCTrace.c
@@ -5,70 +5,72 @@
 #include <unistd.h>
 #include "RCTrace.h"
 
-int g_arr[G_ARR_NUM][2];
+int g_arr[G_ARR_LEFT][2];
 
 void RCTraceDBG(int nLv,char *format,...)
 {	
-	int i;
+	int i, iNum;
+	char strTmpBuf[1000];
+	char *pcTmp;
+	
 	va_list args;
-	va_start(args,format);
-
-	for (i=0; i < G_ARR_NUM; i++) {
-		if (g_arr[i][0] == 0) {
-			break;
-		}
-		else {
-			if (nLv >= g_arr[i][0] && nLv <= g_arr[i][1]) {
-				vprintf(format,args);
-			}
-		}
-	}
+	va_start(args, format);
+	iNum = vsprintf(strTmpBuf, format, args);
 	va_end(args);
+	strTmpBuf[iNum] = '\0';
+	pcTmp = strTmpBuf;
+
+	for (i = 0; i < G_ARR_LEFT; i++) {
+		if (-1 == g_arr[i][0])
+			break;
+		if (nLv >= g_arr[i][0] && nLv <= g_arr[i][1])
+			printf("%s", pcTmp);
+	}
 }
 
 void InitRCTrace(int argc, char **argv)
 {
 	int ch;
-	char *pInitString = NULL;
 
-	ch = getopt(argc, argv, "hd:t:");
+	ch = getopt(argc, argv, "hd:");
 	switch (ch) {
 		case 'd':
-			pInitString = optarg;
+			RCTraceLevelManager(optarg);
+			break;
+		case 'h':
+			printf("请输入这种格式：./test -d\"e:0-2048;aS\"\n\r");
 			break;
 		default:
+			printf("输入-h参数知道help信息\n\r");
 			break;
 	}
-	RCTraceLevelManager(pInitString);
 }
 
 void RCTraceLevelManager(char *pInitString)
 {	
+	int i = 0;
+	char buff[1000];
+
 	char *pstr_start =NULL;
 	char *pstr_end =NULL;
 	char *pToken = NULL;
-	char *pDelimiter = ",";
-	int i = 0;
-	char buff[1000];
 	
 	pstr_start = strstr(pInitString, "e:");
 	pstr_start += 2;
-	pstr_end = strstr(pstr_start, ";aS");
-	*pstr_end = '\0';
+	pstr_end = strstr(pstr_start, ";aS"); //pstr_start的范围在e:和;aS之间
+	*pstr_end = '\0';                     //暂时用不上后面的数据，用结束符增加安全性
 
+	memset(g_arr, -1, sizeof(g_arr));
 	memset(buff, 0, sizeof(buff));
 	strcpy(buff, pstr_start);
 	
-	pToken = strtok(buff, pDelimiter);
+	pToken = strtok(buff, ",");
 	while(pToken) {
-		if(strpbrk(pToken, "-") != NULL) {
+		if(strpbrk(pToken, "-") != NULL) 
 			sscanf(pToken, "%d-%d", &g_arr[i][0], &g_arr[i][1]);
-		}
-		else {
-			g_arr[i][0] = atoi(pToken);
-			g_arr[i][1] = atoi(pToken);
-		}
-		pToken = strtok(NULL, pDelimiter);
+		else
+			g_arr[i][0] = g_arr[i][1] = atoi(pToken);
+		pToken = strtok(NULL, ",");
 		i++;
 	}
 }
